110662f64428a2018a06605710df2a14c5a3c8ff
AkshitBhandariCodes <akshitbhandaricodes@gmail.com>
2025-04-17 13:04:29 +0530

    landing page
    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import Footer from "./components/Footer"
-import React from 'react';
-import ContactPage from './components/Contact-page';
+import Navbar from "./components/navbar"
+import HeroSection from "./components/hero-section"
+import BackgroundAnimation from "./components/background-animation"
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <main className="flex-grow">
-        {/* Your main content goes here */}
-        <div className="h-[500px] flex items-center justify-center bg-[#0a0c14] text-white">
-          <h1 className="text-3xl font-bold">GDG</h1>
-        </div>
-      </main>
-      <ContactPage/>
-      <Footer />
+    <div className="relative min-h-screen overflow-hidden">
+      <BackgroundAnimation />
+      <div className="relative z-10">
+        <Navbar />
+        <main>
+          <HeroSection />
+        </main>
+      </div>
     </div>
   )
 }
 
 export default App
-
